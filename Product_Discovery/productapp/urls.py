a5c5a7434260c866f59df2eb05ee6f3b3e11689a
Dev Nirwal <devnirwal16@gmail.com>
2021-10-23 18:40:12 +0530

    Updated Readme

--- Product_Discovery/productapp/urls.py
+++ Product_Discovery/productapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('login/',views.Login.as_view()),
     path('logout/',views.LogOut.as_view(),name='logout'),
-    path('',views.home,name='home'),
+    path('',views.home,name='home'), 
     path('deepsearch',views.deepsearch,name='deepsearch'),
     path('sparql', views.sparql, name='sparql'),
     path('add', views.add, name="add"),
