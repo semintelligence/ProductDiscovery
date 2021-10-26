c92e68001b55e838f1603dd59d43fde567bcb1d8
Dev Nirwal <devnirwal16@gmail.com>
2021-10-26 23:53:47 +0530

    Fetchec data fron Deep Search form and saved it in a list to start work on product page

--- Product_Discovery/productapp/views.py
+++ Product_Discovery/productapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView , LogoutView
 from django.core.files.storage import FileSystemStorage
+import requests
 
 # Create your views here.
 
@@ -8,6 +9,23 @@ def home(request):
     return render(request, 'index.html')
 
 def deepsearch(request):
+    if(request.method == 'POST'):
+        category  = "=" + request.POST.get('category')
+        brand  = "=" +request.POST.get('brand')
+        ram  = "=" +request.POST.get('ram')
+        operatingsystem  = "=" +request.POST.get('operatingsystem')
+        priceineuros  = "=" +request.POST.get('priceineuros')
+        quantity  = "=" +request.POST.get('quantity')
+        seller  = "=" +request.POST.get('seller')
+        cpu  = "=" +request.POST.get('cpu')
+        gpu  = "=" +request.POST.get('gpu')
+        screensize  = "=" +request.POST.get('screensize')
+        screentype  = "=" +request.POST.get('screentype')
+        storage  = "=" +request.POST.get('storage')
+        url = "https://product-discovery-service.herokuapp.com/recommendProduct?Category"+category+"&Brand" + brand + "&RAM" + ram + "&OperatingSystem" +operatingsystem +"&PriceInEuros" + priceineuros + "&Quantity" + quantity + "&Seller" + seller + "&CPU" + cpu + "&GPU" + gpu + "&ScreenSize" + screensize + "&ScreenType"+screentype+"&Storage" + storage
+        res = requests.get(url)
+        resList = res.text.split(',')
+        return render(request,'deepsearchproduct.html')
     return render(request, 'deepsearch.html')
 
 def sparql(request):
