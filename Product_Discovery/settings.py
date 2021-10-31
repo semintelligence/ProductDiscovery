f54e708ceb0ab52f58ebf250a0888ce82a4d4197
Dev Nirwal <devnirwal16@gmail.com>
2021-10-31 19:55:08 +0530

    heroku

--- Product_Discovery/settings.py
+++ Product_Discovery/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(q^gd)uc#=smksp#-brq96d&!!w$7syafmyn0*xo^q#-d6sz2w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://prod-dis.herokuapp.com/"]
 
 
 # Application definition
